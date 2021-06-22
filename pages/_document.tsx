262d8053e0f93537a75165cb30179043f1bba11f
Mark Lawlor <mwlawlor@gmail.com>
2021-06-22 20:00:30 +1000

    Updated patch and document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,4 @@
 import Document from "@expo/next-adapter/document";
-import { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
-class CustomDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default CustomDocument;
+export default Document;
